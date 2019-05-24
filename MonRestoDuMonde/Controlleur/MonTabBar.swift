2e65e09aca14178cb747d3fb34db4347eab97c75
captain web <contact@captain-web.fr>
2019-05-24 11:19:09 +0200

    tabbar custom

---
+++ MonRestoDuMonde/Controlleur/MonTabBar.swift
@@ -0,0 +1,34 @@
+//
+//  MonTabBar.swift
+//  MonRestoDuMonde
+//
+//  Created by florian kindt on 24/05/2019.
+//  Copyright © 2019 captain web. All rights reserved.
+//
+
+import UIKit
+
+class MonTabBar: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabBar.barTintColor = GRIS_TRES_FONCE
+        tabBar.tintColor = GRIS_TRES_CLAIRE
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MonRestoDuMonde/Utilitaire/Constantes.swift
@@ -0,0 +1,13 @@
+//
+//  Constantes.swift
+//  MonRestoDuMonde
+//
+//  Created by florian kindt on 24/05/2019.
+//  Copyright © 2019 captain web. All rights reserved.
+//
+
+import UIKit
+
+let NOIR = UIColor.black
+let GRIS_TRES_FONCE = UIColor(red: 30/255, green: 30/255, blue: 30/255, alpha: 1)
+let GRIS_TRES_CLAIRE = UIColor(red: 240/255, green: 240/255, blue: 240/255, alpha: 1)
