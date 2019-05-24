f332758c6d248430638cb5a2dba11219d943fe28
captain web <contact@captain-web.fr>
2019-05-24 15:46:34 +0200

    ajout collecitonVie

---
+++ MonRestoDuMonde/Vue/MenuCell.swift
@@ -0,0 +1,13 @@
+//
+//  MenuCell.swift
+//  MonRestoDuMonde
+//
+//  Created by florian kindt on 24/05/2019.
+//  Copyright © 2019 captain web. All rights reserved.
+//
+
+import UIKit
+
+class MenuCell: UICollectionViewCell {
+    
+}
