5295e31bbc366c8177f8a912b9bde9044a62fb9a
captain web <contact@captain-web.fr>
2019-05-24 15:52:28 +0200

    Ajout classe plat

---
+++ MonRestoDuMonde/Modele/Enums.swift
@@ -0,0 +1,15 @@
+//
+//  Enums.swift
+//  MonRestoDuMonde
+//
+//  Created by florian kindt on 24/05/2019.
+//  Copyright © 2019 captain web. All rights reserved.
+//
+
+import Foundation
+
+enum Type {
+    case entree
+    case platPrincipal
+    case dessert
+}
---
+++ MonRestoDuMonde/Modele/Plat.swift
@@ -0,0 +1,42 @@
+//
+//  Plat.swift
+//  MonRestoDuMonde
+//
+//  Created by florian kindt on 24/05/2019.
+//  Copyright © 2019 captain web. All rights reserved.
+//
+
+import UIKit
+
+class Plat {
+    
+    private var _type: Type
+    private var _pays: String
+    private var _desc: String
+    private var _image: UIImage
+    
+    var type: Type {
+        return _type
+    }
+    
+    var pays: String {
+        return _pays
+    }
+    
+    var desc: String {
+        return _desc
+    }
+    
+    var image: UIImage {
+        return _image
+    }
+    
+    init(type: Type, pays: String, desc: String, image: UIImage) {
+        self._type = type
+        self._pays = pays
+        self._desc = desc
+        self._image = image
+    }
+    
+    
+}
